4c35833622b0dbdf84fd0c45f8638d5ae4b4c5ae
NikitaGundorin <djnig@yandex.ru>
2019-09-22 16:09:40 +0300

    Added controllers

---
+++ BugTrackingSystem/Controllers/AccountController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using BugTrackingSystem.Models;
+using BugTrackingSystem.ViewModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace BugTrackingSystem.Controllers
+{
+    [Route("api/account")]
+    public class AccountController : Controller
+    {
+        private readonly BugTrackingSystemContext db;
+
+        public AccountController(BugTrackingSystemContext context)
+        {
+            db = context;
+        }
+
+        [HttpPost("token")]
+        public IActionResult Token([FromBody] LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var username = model.UserName;
+                var password = model.Password;
+
+                var result = Authenticate(username, password);
+                if (result == null)
+                {
+                    return BadRequest(new { message = "Username or password is incorrect" });
+                }
+                return Ok(result);
+            }
+            return BadRequest(new { message = "Username or password is incorrect" });
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = await db.Users.FirstOrDefaultAsync(u => ((u.Email == model.Email) || (u.UserName == model.UserName)));
+                if (user == null)
+                {
+                    db.Users.Add(new User { Email = model.Email, Password = model.Password, UserName = model.UserName, Role = "user" });
+                    await db.SaveChangesAsync();
+
+                    var result = Authenticate(model.UserName, model.Password);
+
+                    return Ok(result);
+                }
+                    return BadRequest(new { message = "User already exist" });
+            }
+                return BadRequest(new { message = "Invalid data" });
+        }
+
+        private ClaimsIdentity GetIdentity(string username, string password)
+        {
+            User user = db.Users.FirstOrDefault(x => (x.UserName == username || x.Email == username) && x.Password.Equals(password));
+            if (user != null)
+            {
+                var claims = new List<Claim>
+            {
+            new Claim(ClaimsIdentity.DefaultNameClaimType, user.UserName),
+            new Claim(ClaimsIdentity.DefaultRoleClaimType, user.Role)
+            };
+                ClaimsIdentity claimsIdentity =
+                new ClaimsIdentity(claims, "Token", ClaimsIdentity.DefaultNameClaimType,
+                ClaimsIdentity.DefaultRoleClaimType);
+                return claimsIdentity;
+            }
+
+            return null;
+        }
+
+        private object Authenticate(string userName, string password)
+        {
+            var identity = GetIdentity(userName, password);
+            if (identity == null)
+            {
+                return null;
+            }
+
+            var now = DateTime.UtcNow;
+            var jwt = new JwtSecurityToken(
+                            issuer: AuthOptions.ISSUER,
+                            audience: AuthOptions.AUDIENCE,
+                            notBefore: now,
+                            claims: identity.Claims,
+                            expires: now.Add(TimeSpan.FromMinutes(AuthOptions.LIFETIME)),
+                            signingCredentials: new SigningCredentials(AuthOptions.GetSymmetricSecurityKey(), SecurityAlgorithms.HmacSha256));
+            var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
+
+            var response = new
+            {
+                access_token = encodedJwt,
+                role = identity.Claims.Last().Value
+            };
+
+            return response;
+        }
+    }
+}
---
+++ BugTrackingSystem/Controllers/HomeController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BugTrackingSystem.Models;
+using BugTrackingSystem.ViewModels;
+using BugTrackingSystem.Models.Repositories;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
+
+namespace BugTrackingSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [Authorize]
+    public class HomeController : Controller
+    {
+        private readonly BugTrackingSystemContext db;
+        private readonly BugRepository bugRepository;
+
+        public HomeController(BugTrackingSystemContext context)
+        {
+            db = context;
+            bugRepository = new BugRepository(context);
+        }
+
+        [HttpGet("index")]
+        public async Task<IActionResult> Index()
+        {
+            List<BugViewModel> bugs = await db.Bugs.Select(b => new BugViewModel
+            {
+                Id = b.Id,
+                CreationDate = b.CreationDate.ToString("dd.MM.yyy hh:mm"),
+                ShortDescription = b.ShortDescription,
+                FullDescription = b.FullDescription,
+                Importance = b.Importance.Name,
+                Priority = b.Priority.Name,
+                Status = b.Status.Name,
+                UserName = b.User.UserName
+            }).ToListAsync();
+
+            return Ok(bugs);
+        }
+
+        [HttpPost("updatebugstatus")]
+        public async Task<IActionResult> UpdateBugStatus(int id, int newBugStatusId, string comment)
+        {
+
+            User user = await db.Users.FirstOrDefaultAsync(u => (u.Email == User.Identity.Name) || (u.UserName == User.Identity.Name));
+
+            await bugRepository.UpdateBugStatusAsync(id, newBugStatusId, comment, user.Id);
+
+            return Ok();
+        }
+
+        [HttpGet("addbug")]
+        public IActionResult AddBug()
+        {
+            List<List<object>> parameters = new List<List<object>>();
+            List<object> importance = new List<object>();
+            foreach (Importance i in db.Importances)
+            {
+                importance.Add(i);
+            }
+            parameters.Add(importance);
+
+            List<object> priority = new List<object>();
+            foreach (Priority p in db.Priorities)
+            {
+                priority.Add(p);
+            }
+            parameters.Add(priority);
+
+            return Ok(parameters);
+        }
+
+        [HttpPost("addbug")]
+        public async Task<IActionResult> AddBug([FromBody] CreateBugViewModel bug)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = await db.Users.FirstOrDefaultAsync(u => (u.Email == User.Identity.Name) || (u.UserName == User.Identity.Name));
+
+                await bugRepository.CreateBug(bug, user);
+                return Ok();
+            }
+            return BadRequest(new { message = "Parameters are incorrect" });
+        }
+
+        [HttpGet("getbugchangelog")]
+        public async Task<IActionResult> GetBugChangelog(int bugId)
+        {
+            List<BugChangelogViewModel> bugChangelogs = await bugRepository.GetBugChangelogs(bugId);
+
+            return Ok(bugChangelogs);
+        }
+    }
+}
